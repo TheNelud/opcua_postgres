1f53930de3d17dfd751ca56c3abf364ffb4c6908
TheNelud <riwok@bk.ru>
2022-06-23 11:26:50 +0300

    update

--- client/main.py
+++ client/main.py
@@ -13,16 +13,16 @@ if __name__ == "__main__":
     
     logger.info("Программа стартует")
     config = get_config() 
-    # print(config['ather_setting']['rate_data_to_alpha'])
-    # create_client_post_alpha()
+   
 
-    # client_5min = config['rate_5_min']['cl_table']
-    # client_hour = config['rate_1_hour']['cl_table']
-    # client_day = config['rate_1_day']['cl_table']
-    # # настройка по расписанию postgres -> opc -> postgres
-    # schedule.every(5).minutes.at(":00").do(create_client_post_to_post, client_5min)
-    # schedule.every().hour.at(f"{config['rate_1_hour']['cl_rate']}").do(create_client_post_to_post, client_hour)
-    # schedule.every().day.at(f"{config['rate_1_day']['cl_rate']}").do(create_client_post_to_post, client_day)
+    client_5min = config['rate_5_min']['cl_table']
+    client_hour = config['rate_1_hour']['cl_table']
+    client_day = config['rate_1_day']['cl_table']
+    
+    # настройка по расписанию postgres -> opc -> postgres
+    schedule.every(5).minutes.at(":00").do(create_client_post_to_post, client_5min)
+    schedule.every().hour.at(f"{config['rate_1_hour']['cl_rate']}").do(create_client_post_to_post, client_hour)
+    schedule.every().day.at(f"{config['rate_1_day']['cl_rate']}").do(create_client_post_to_post, client_day)
 
     #настройка по расписанию postgres -> opc
     schedule.every(int(config['ather_setting']['rate_data_to_alpha'])).seconds.do(create_client_post_alpha)
